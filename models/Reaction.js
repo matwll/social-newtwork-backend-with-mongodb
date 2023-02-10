a2e9501a4a4794c6545b7999b7c4a444cd93080d
Matthew Williams <maw01091989@gmail.com>
2023-02-09 18:26:54 -0800

    added models

---
+++ models/Reaction.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: new Schema.Types.ObjectId,
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max_length: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+const Reaction = model('reaction', reactionSchema);
+
+module.exports = Reaction;
